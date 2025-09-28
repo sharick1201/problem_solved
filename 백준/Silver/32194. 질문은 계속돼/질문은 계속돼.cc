4368ab58cedd9b376cb2373f6e036140a4436376
Sieun Park <124417560+sharick1201@users.noreply.github.com>
2025-09-28 12:19:57 +0900

    [Silver II] Title: 질문은 계속돼, Time: 52 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/32194. 질문은 계속돼/질문은 계속돼.cc
+++ 백준/Silver/32194. 질문은 계속돼/질문은 계속돼.cc
@@ -1,56 +1,47 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
 using namespace std;
 
-int n;
+int N;
+int sum[200002]; // i번째 질문까지의 yes의 개수
 
-int main()
-{
-    ios::sync_with_stdio(false);
+int main() {
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    cin >> n;
+    cin >> N;
+    sum[1] = 1;
 
-    vector<int> yes(n + 2, 0); 
-    vector<int> no(n + 2, 0); 
-
-    yes[1] = 1;
-
-    int type, x, y;
-    for (int i = 2; i <= n + 1; i++)
-    {
+    for (int i = 2; i <= N+1; i++) { 
+        int type, x, y;
         cin >> type >> x >> y;
 
-        if (type == 1)
-        {
-            if ((yes[y] - yes[x - 1]) == (y - x + 1))
-            { 
+        int count = sum[y] - sum[x - 1];
+
+        if (type == 1) { 
+            if (count == (y - x + 1))
+            {
                 cout << "Yes\n";
-                yes[i] = yes[i - 1] + 1;
-                no[i] = no[i - 1];
+                sum[i] = sum[i-1] + 1;
             }
             else
             {
                 cout << "No\n";
-                no[i] = no[i - 1] + 1;
-                yes[i] = yes[i - 1];
+                sum[i] = sum[i - 1];
             }
-        }
-        else if (type == 2)
-        {
-            if ((no[y] - no[x - 1]) == (y - x + 1))
-            { 
+        } else {
+
+            if (count == 0)
+            {
                 cout << "Yes\n";
-                yes[i] = yes[i - 1] + 1;
-                no[i] = no[i - 1];
+                sum[i] = sum[i - 1] + 1;
             }
             else
             {
-                cout << "No\n";
-                no[i] = no[i - 1] + 1;
-                yes[i] = yes[i - 1];
+                cout <<"No\n";
+                sum[i] = sum[i - 1];
             }
         }
     }
