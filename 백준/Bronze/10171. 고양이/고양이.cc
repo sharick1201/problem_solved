ebcec0cce1ce40d4341daa38c247578f7f5d3b6d
Sieun Park <124417560+sharick1201@users.noreply.github.com>
2025-10-04 19:31:58 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|";
+    return 0;
+}
