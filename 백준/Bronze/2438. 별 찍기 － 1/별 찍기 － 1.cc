647cffd49f9d4b4f3bebab7997acb574df90d15d
Sieun Park <124417560+sharick1201@users.noreply.github.com>
2025-10-07 11:22:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
