71edd91de31be9c38fe46e75480a1eb31c0eead0
Sieun Park <124417560+sharick1201@users.noreply.github.com>
2025-10-08 16:43:11 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int a, b;
+    while (cin >> a >> b)  {
+        if (a == 0 && b == 0) break;
+        cout << a + b << "\n";
+    }
+    
+    return 0;
+}
