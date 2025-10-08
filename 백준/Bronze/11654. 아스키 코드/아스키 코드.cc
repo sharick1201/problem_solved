b1c1678f7879a6a42d922bbd3f5e54ea1fc765d2
Sieun Park <124417560+sharick1201@users.noreply.github.com>
2025-10-08 16:44:03 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    char input;
+    cin >> input;
+    cout << (int)input;
+    return 0;
+}
