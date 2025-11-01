808e9d2a49b9dbfc61b47371d0e7253aef5403d1
Sieun Park <124417560+sharick1201@users.noreply.github.com>
2025-11-01 16:07:35 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 300 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -6,32 +6,6 @@ int N;
 int arr[500'001];
 int M;
 
-int lower_idx(int target) {
-    int st = 0;
-    int en = N;
-    int mid; 
-    while (st < en) {
-        mid = (st + en) / 2;
-        if (arr[mid] >= target) en = mid;
-        else st = mid + 1;
-    }
-    
-    return st;
-}
-
-int upper_idx(int target) {
-    int st = 0;
-    int en = N;
-    int mid; 
-    while (st < en) {
-        mid = (st + en) / 2;
-        if (arr[mid] > target) en = mid;
-        else st = mid + 1;
-    }
-    
-    return st;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -46,7 +20,7 @@ int main() {
     for (int i = 0; i < M; ++i) {
         int tar;
         cin >> tar;
-        cout << upper_idx(tar) - lower_idx(tar) << " ";
+        cout << upper_bound(arr, arr+N, tar) - lower_bound(arr, arr+N, tar) << " ";
     }
     return 0;
 }
