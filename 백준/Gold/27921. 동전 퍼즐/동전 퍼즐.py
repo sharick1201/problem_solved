e0af1bfc2a8546492c993b13f75d2d589f06e513
Sieun Park <124417560+sharick1201@users.noreply.github.com>
2026-02-15 12:12:38 +0900

    [Gold V] Title: 동전 퍼즐, Time: 96 ms, Memory: 110956 KB -BaekjoonHub

--- 백준/Gold/27921. 동전 퍼즐/동전 퍼즐.py
+++ 백준/Gold/27921. 동전 퍼즐/동전 퍼즐.py
@@ -1,28 +1,35 @@
 N1, M1 = map(int, input().split())
-arr1 = list(input() for _ in range(N1))
+arr1 = list(list(input()) for _ in range(N1))
 N2, M2 = map(int, input().split())
-arr2 = list(input() for _ in range(N2))
-
-N = max(N1, N2)
-M = max(M1, M2)
+arr2 = list(list(input()) for _ in range(N2))
 
 coin_cnt = sum(arr1[i].count('O') for i in range(N1))
 max_overlap_cnt = 0
 
+# 다른 사람들처럼 그리드 만들어서 해보기
+# N = (N2 - 1) + N1 + (N2 - 1)
+N = 2 * N2 + N1 - 2
+M = 2 * M2 + M1 - 2
+grid = [['.'] * M for _ in range(N)]
 
-for diff_i in range(-N+1, N):
-    for diff_j in range(-M+1, M):
+# 중앙에 arr1 집어넣기
+for i in range(N1):
+    for j in range(M1):
+        grid[i + N2 - 1][j + M2 - 1] = arr1[i][j]
 
+# arr2 이동시켜가면서 해보기
+for si in range(N):
+    for sj in range(M):
         cnt = 0
 
         # 이동된 lst2 <-> lst1 비교하면서 동전 겹치는 부분 카운트
-        for i in range(N1):
-            for j in range(M1):
-                ny = i + diff_i
-                nx = j + diff_j
-                if not (0 <= ny < N2 and 0 <= nx < M2):
+        for i in range(N2):
+            for j in range(M2):
+                ny = i + si
+                nx = j + sj
+                if not (0 <= ny < N and 0 <= nx < M):
                     continue
-                if arr1[i][j] == 'O' and arr2[ny][nx] == 'O':
+                if grid[ny][nx] == 'O' and arr2[i][j] == 'O':
                     cnt += 1
 
         max_overlap_cnt = max(max_overlap_cnt, cnt)
