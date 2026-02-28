f5d6bcf1e06304f2ff804d1f19de7eb4b375a5a7
Sieun Park <124417560+sharick1201@users.noreply.github.com>
2026-02-28 15:07:49 +0900

    [Gold III] Title: 주사위 굴리기 2, Time: 184 ms, Memory: 115488 KB -BaekjoonHub

--- 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
+++ 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
@@ -1,70 +1,15 @@
-"""
-출력: 각 이동에서 획득하는 점수의 "합"
+# 리팩토링 1
+# 주사위 회전 로직 변경 1: 철현님 방식 - 뉴 다이스 한 번에 만드는 방식 (내가 방만하게 짠 코드를 한 번 더 정리한 느낌. 깔끔)
+# ans type 변경: 디버깅용으로 배열로 해둠 -> 정답 맞췄으니까 효율성을 위해 정수로 바꿈  188 ms -> 196 ms (???? 머쓱)
 
-
-1. 다음 이동 방향에 칸이 있다면 -> ny, nx로 간다.
-   다음 이동 방향에 칸이 없다면 -> 이동 방향 "반대"로 한 다음 그 반대칸으로 간다.
-
-    - 주사위 바닥이 있는 곳 dice_y, dice_x 갱신
-    - 주사위 위치에 맞게 회전
-
-2. 주사위가 도착한 칸(arr[ny][nx]) 에 대한 점수 획득
-    - 점수란? 값이 arr[ny][nx] 인 칸 dfs해서 방문한 칸 갯수 * arr[ny][nx]
-
-
-3. 주사위의 다음 이동 방향 결정
-    주사위의 아랫면에 있는 정수 A(주사위 눈) 와 주사위가 있는 칸 (x, y)에 있는 정수를 비교
-        A > B인 경우 이동 방향을 90도 시계 방향으로 회전시킨다.
-        A < B인 경우 이동 방향을 90도 반시계 방향으로 회전시킨다.
-        A = B인 경우 이동 방향에 변화는 없다.
-
-
-주사위를 회전시키는 게 아님. 이동 방향을 회전시키는 것
-방문한 칸의 점수가 사라지지는 않음
-"""
-
-# 주사위 회전에 따라 달라지는 눈-위치를 다시 매핑
-# return 없음
 def roll_dice(dice_dir):
     global dice
-    new_dice = [0] * 6
-    # 오른쪽으로
-    if dice_dir == 0:
-        new_dice[0] = dice[5]  # 바닥  3
-        new_dice[1] = dice[1]  # 상   2
-        new_dice[2] = dice[4]  # 천장  4
-        new_dice[3] = dice[3]  # 하    5
-        new_dice[4] = dice[0]  # 왼쪽  6
-        new_dice[5] = dice[2]  # 오른쪽 1
-
-    # 아래로
-    elif dice_dir == 1:
-        new_dice[0] = dice[3]  # 바닥  5
-        new_dice[1] = dice[0]  # 상    6
-        new_dice[2] = dice[1]  # 천장  2
-        new_dice[3] = dice[2]  # 하    1
-        new_dice[4] = dice[4]  # 왼쪽  4
-        new_dice[5] = dice[5]  # 오른쪽 3
-
-    # 왼쪽으로
-    elif dice_dir == 2:
-        new_dice[0] = dice[4]  # 바닥  4
-        new_dice[1] = dice[1]  # 상    2
-        new_dice[2] = dice[5]  # 천장  3
-        new_dice[3] = dice[3]  # 하    5
-        new_dice[4] = dice[2]  # 왼쪽  1
-        new_dice[5] = dice[0]  # 오른쪽 6
-
-    # 위로
-    elif dice_dir == 3:
-        new_dice[0] = dice[1]  # 바닥  2
-        new_dice[1] = dice[2]  # 상    1
-        new_dice[2] = dice[3]  # 천장  5
-        new_dice[3] = dice[0]  # 하    6
-        new_dice[4] = dice[4]  # 왼쪽  4
-        new_dice[5] = dice[5]  # 오른쪽 3
-
-    dice = new_dice
+    rolled_dice = [[dice[5], dice[1], dice[4], dice[3], dice[0], dice[2]],
+                   [dice[3], dice[0], dice[1], dice[2], dice[4], dice[5]],
+                   [dice[4], dice[1], dice[5], dice[3], dice[2], dice[0]],
+                   [dice[1], dice[2], dice[3], dice[0], dice[4], dice[5]]]
+    
+    dice = rolled_dice[dice_dir]
 
 
 # 값이 arr[dice_y][dice_x]랑 같은 칸의 갯수를 세는 함수
@@ -109,14 +54,10 @@ dice = [6, 2, 1, 5, 4, 3]
 dir_idx = 0
 dice_y, dice_x = 0, 0
 
-ans = []  # 디버깅 하기 쉽게 리스트로 관리했음
+ans = 0
 
 for _ in range(K):
     """ 1. 주사위 이동 """
-    # 1. 다음 이동 방향에 칸이 있다면 -> ny, nx로 간다.
-    #    다음 이동 방향에 칸이 없다면 -> 이동 방향 "반대"로 한 다음! 그 반대칸으로 간다.
-    #     - 주사위 바닥이 있는 곳 dice_y, dice_x 갱신
-    #     - 주사위 위치에 맞게 회전
     ny = dice_y + dy[dir_idx]
     nx = dice_x + dx[dir_idx]
 
@@ -134,24 +75,17 @@ for _ in range(K):
     roll_dice(dir_idx)
 
     """ 2. 점수 계산 """
-    # 2. 주사위가 도착한 칸(arr[ny][nx]) 에 대한 점수 획득
-    #     - 점수란? 값이 arr[ny][nx] 인 칸 dfs해서 방문한 칸 갯수 * arr[ny][nx]
     visited = [[False] * M for _ in range(N)]
     target_val = arr[dice_y][dice_x]
     same_val_cnt = 0
     cnt_by_dfs(dice_y, dice_x)
 
-    ans.append(same_val_cnt * target_val)
+    ans += same_val_cnt * target_val
 
     """ 3. 주사위의 다음 이동 방향 결정 """
-    # 3. 주사위의 아랫면에 있는 정수 A(주사위 눈) 와 주사위가 있는 칸 (x, y)에 있는 정수를 비교
-    #         A > B인 경우 이동 방향을 90도 시계 방향으로 회전시킨다.
-    #         A < B인 경우 이동 방향을 90도 반시계 방향으로 회전시킨다.
-    #         A = B인 경우 이동 방향에 변화는 없다.
     if dice[0] > arr[dice_y][dice_x]:
         dir_idx = (dir_idx + 1) % 4
     elif dice[0] < arr[dice_y][dice_x]:
         dir_idx = (dir_idx - 1) % 4
 
-print(sum(ans))
-
+print(ans)
